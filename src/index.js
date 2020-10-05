2ed07d20187c68008a1c6c43159aaf444fd75423
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-05 12:22:09 +1100

    use axios instance and baseURL

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = "http://jsonplaceholder.typicode.com";
+
+
 axios.interceptors.request.use(request => {
     console.log(request);
 
