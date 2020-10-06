9ad9abd45b1427251afb8f5708387042ae0ab100
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-06 14:22:34 +1100

    add nested route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import {Route, NavLink} from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import './Blog.css';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
     render() {
-        
+
         return (
-            <div className = "Blog">
-             <header>
-                 <nav>
-                     <ul>
-                         <li><NavLink 
-                                to = "/" 
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
                                 exact
-                                activeClassName = "active">Home</NavLink></li>
-                         <li><NavLink to = {{
-                             pathname: '/new-post',
-                             hash: '#submit',
-                             search: '?quick-submit=true'
-                         }}>New Post</NavLink></li>
-                     </ul>
-                 </nav>
-             </header>
-             <Route path = "/" exact component = {Posts} />
-             <Route path = "/new-post" component = {NewPost} />
-             <Route path = "/posts/:id" exact component = {FullPost} />
-             
+                                activeClassName="active">Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                </Switch>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from '../../../axios';
+import { Route } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
 
     state = {
         posts: []
     }
-
-    postSelectedHandler(id) {
-        this.setState({ selectedPostId: id });
+ 
+    postSelectedHandler = (id) => {
+        this.props.history.push({
+            pathname: '/posts/' + id
+        });
+        //  this.props.history.push('/posts' + id);
     }
 
     componentDidMount() {
@@ -39,22 +43,25 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to ={'/posts' + post.id} key={post.id}>
-                        <Post
-                            
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}
-                        />
-                    </Link>
+                    // <Link to ={'/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                    //  </Link>
                 );
             });
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
         );
     }
 }
